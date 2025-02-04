4aab889e7f61a25ca63fe1989e01e5026e24bf4e
Magomed <mmusadaev.mm@gmail.com>
2025-02-04 20:56:55 +0300

    adding semi-final payment0.4

--- internal/entities/key_db.go
+++ internal/entities/key_db.go
@@ -4,8 +4,8 @@ import "time"
 
 // Key - структура для хранения информации о ключах доступа пользователя.
 type Key struct {
-	UserTgID  int       `db:"user_id"`    // ID пользователя в Telegram (связан с таблицей пользователей)
-	Key       string    `db:"key"`        // Уникальный ключ доступа
-	CreatedAt time.Time `db:"created_at"` // Дата и время создания ключа
-	ExpiresAt time.Time `db:"expires_at"` // Дата и время истечения срока действия ключа
+	UserTgID  int       `db:"telegram_id"` // Используем telegram_id в качестве внешнего ключа
+	Key       string    `db:"key"`
+	CreatedAt time.Time `db:"created_at"`
+	ExpiresAt time.Time `db:"expires_at"`
 }
