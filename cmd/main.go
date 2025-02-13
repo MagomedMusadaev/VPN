de921347bf46c466de432aff3f9ecf0458f09100
said <said@mail.ru>
2025-02-13 19:24:26 +0300

    //test deploy

--- cmd/main.go
+++ cmd/main.go
@@ -99,7 +99,6 @@ func initBotAPI() (*tgbotapi.BotAPI, tgbotapi.UpdatesChannel, error) {
 	return botAPI, updates, nil
 }
 
-//test deploy delete
 //TODO
 // ХРАНИМ:
 // chat_id BIGINT UNIQUE NOT NULL,            -- Уникальный идентификатор пользователя в Telegram
